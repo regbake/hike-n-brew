52771871c430fe5c4ae542d98b82a9313ac3c6c2
regbake <regbake@gmail.com>
2017-11-02 12:05:35 -0700

    moar final changes

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ var flash = require('connect-flash');
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mern-local-auth');
+// mongoose.connect('mongodb://localhost/mern-local-auth');
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
-var index = require('./routes/index');
+// var index = require('./routes/index');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 var search = require("./routes/search");
@@ -32,7 +33,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//commented below out for heroku postbuild
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 /* Configure the express-session...
  * [secret]: A string used to "sign" the session ID cookie, which makes it unique
@@ -61,10 +64,14 @@ app.use(function(req, res, next) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', index);
+// app.use('/', index);
 app.use('/users', users);
 app.use('/auth', auth);
 app.use("/search", search);
+
+app.get('*', function(req, res, next) {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler - commented out
 // app.use(function(req, res, next) {
